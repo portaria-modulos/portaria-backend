8d0554f26d3e06cf33a70db2be9a1b962fc12581
Elivandro Lopes <elivandro78@gmail.com>
2026-06-22 12:22:31 -0300

    subindo teste no git

--- src/main/java/com/portariacd/modulos/Moduloportaria/infrastructure/persistence/history/HistoryEntradaEntity.java
+++ src/main/java/com/portariacd/modulos/Moduloportaria/infrastructure/persistence/history/HistoryEntradaEntity.java
@@ -7,17 +7,12 @@ import com.portariacd.modulos.Moduloportaria.infrastructure.persistence.Visitant
 import com.portariacd.modulos.Moduloportaria.infrastructure.persistence.registroVisitante.RegistroVisitantePortariaEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 @Entity
 @Table(name="history")
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 public class HistoryEntradaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -82,5 +77,173 @@ public class HistoryEntradaEntity {
         this.ficalIdSaida = r.getSaidaVisitante().getFicalSaidaId();
         this.nomeFiscaSaida = r.getSaidaVisitante().getNomeFiscal();
         this.dataSaida = r.getSaidaVisitante().getDataSaida();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getRegistroPortariaId() {
+        return registroPortariaId;
+    }
+
+    public void setRegistroPortariaId(long registroPortariaId) {
+        this.registroPortariaId = registroPortariaId;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public VisitanteEntity getVisitante() {
+        return visitante;
+    }
+
+    public void setVisitante(VisitanteEntity visitante) {
+        this.visitante = visitante;
+    }
+
+    public String getPlacaVeiculo() {
+        return placaVeiculo;
+    }
+
+    public void setPlacaVeiculo(String placaVeiculo) {
+        this.placaVeiculo = placaVeiculo;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusPortaria getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPortaria status) {
+        this.status = status;
+    }
+
+    public UsuarioEntity getCriador() {
+        return criador;
+    }
+
+    public void setCriador(UsuarioEntity criador) {
+        this.criador = criador;
+    }
+
+    public Integer getFilialSolicitado() {
+        return filialSolicitado;
+    }
+
+    public void setFilialSolicitado(Integer filialSolicitado) {
+        this.filialSolicitado = filialSolicitado;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getProtocolo() {
+        return protocolo;
+    }
+
+    public void setProtocolo(String protocolo) {
+        this.protocolo = protocolo;
+    }
+
+    public String getBloco() {
+        return bloco;
+    }
+
+    public void setBloco(String bloco) {
+        this.bloco = bloco;
+    }
+
+    public LocalDateTime getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(LocalDateTime dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public LocalDateTime getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(LocalDateTime dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public Long getFicalIdEntrada() {
+        return ficalIdEntrada;
+    }
+
+    public void setFicalIdEntrada(Long ficalIdEntrada) {
+        this.ficalIdEntrada = ficalIdEntrada;
+    }
+
+    public Long getFicalIdSaida() {
+        return ficalIdSaida;
+    }
+
+    public void setFicalIdSaida(Long ficalIdSaida) {
+        this.ficalIdSaida = ficalIdSaida;
+    }
+
+    public String getNomeFiscaEntrada() {
+        return nomeFiscaEntrada;
+    }
+
+    public void setNomeFiscaEntrada(String nomeFiscaEntrada) {
+        this.nomeFiscaEntrada = nomeFiscaEntrada;
+    }
+
+    public String getNomeFiscaSaida() {
+        return nomeFiscaSaida;
+    }
+
+    public void setNomeFiscaSaida(String nomeFiscaSaida) {
+        this.nomeFiscaSaida = nomeFiscaSaida;
+    }
+
+    public String getImagemEntrada() {
+        return imagemEntrada;
+    }
+
+    public void setImagemEntrada(String imagemEntrada) {
+        this.imagemEntrada = imagemEntrada;
+    }
+
+    public String getImagemSaida() {
+        return imagemSaida;
+    }
+
+    public void setImagemSaida(String imagemSaida) {
+        this.imagemSaida = imagemSaida;
     }
 }
