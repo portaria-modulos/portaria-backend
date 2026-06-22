da8365b7ab02f95ee02a91e59eb2fa060ea954d5
Elivandro Lopes <elivandro78@gmail.com>
2026-06-22 12:05:40 -0300

    subindo teste no app

--- src/main/java/com/portariacd/modulos/Moduloportaria/domain/models/controleDeChaves/EntregaChaveHistory.java
+++ src/main/java/com/portariacd/modulos/Moduloportaria/domain/models/controleDeChaves/EntregaChaveHistory.java
@@ -1,10 +1,19 @@
 package com.portariacd.modulos.Moduloportaria.domain.models.controleDeChaves;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "ENTREGA_CHAVE_HISTORY")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EntregaChaveHistory {
 
     @Id
@@ -34,104 +43,6 @@ public class EntregaChaveHistory {
     private String status;
 
     // --- GETTERS E SETTERS ---
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getBlocoChaves() {
-        return blocoChaves;
-    }
-
-    public void setBlocoChaves(long blocoChaves) {
-        this.blocoChaves = blocoChaves;
-    }
-
-    public String getNomeColaborador() {
-        return nomeColaborador;
-    }
-
-    public void setNomeColaborador(String nomeColaborador) {
-        this.nomeColaborador = nomeColaborador;
-    }
-
-    public String getMatriculaColaborador() {
-        return matriculaColaborador;
-    }
-
-    public void setMatriculaColaborador(String matriculaColaborador) {
-        this.matriculaColaborador = matriculaColaborador;
-    }
-
-    public OffsetDateTime getDataHoraRetirada() {
-        return dataHoraRetirada;
-    }
-
-    public void setDataHoraRetirada(OffsetDateTime dataHoraRetirada) {
-        this.dataHoraRetirada = dataHoraRetirada;
-    }
-
-    public LocalDateTime getDataHoraDevolucao() {
-        return dataHoraDevolucao;
-    }
-
-    public void setDataHoraDevolucao(LocalDateTime dataHoraDevolucao) {
-        this.dataHoraDevolucao = dataHoraDevolucao;
-    }
-
-    public String getUsuarioPortariaRetirada() {
-        return usuarioPortariaRetirada;
-    }
-
-    public void setUsuarioPortariaRetirada(String usuarioPortariaRetirada) {
-        this.usuarioPortariaRetirada = usuarioPortariaRetirada;
-    }
-
-    public String getUsuarioPortariaDevolucao() {
-        return usuarioPortariaDevolucao;
-    }
-
-    public void setUsuarioPortariaDevolucao(String usuarioPortariaDevolucao) {
-        this.usuarioPortariaDevolucao = usuarioPortariaDevolucao;
-    }
-
-    public Long getUsuarioIdRetirada() {
-        return usuarioIdRetirada;
-    }
-
-    public void setUsuarioIdRetirada(Long usuarioIdRetirada) {
-        this.usuarioIdRetirada = usuarioIdRetirada;
-    }
-
-    public Long getUsuarioIdDevolucao() {
-        return usuarioIdDevolucao;
-    }
-
-    public void setUsuarioIdDevolucao(Long usuarioIdDevolucao) {
-        this.usuarioIdDevolucao = usuarioIdDevolucao;
-    }
-
-    public void setFilialId(Long filialId) {
-        this.filialId = filialId;
-    }
-
-    public Long getFilialId() {
-        return filialId;
-    }
-
-    public void setBlocoChaves(Long blocoChaves) {
-        this.blocoChaves = blocoChaves;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getStatus() {
-        return status;
-    }
 
 }
